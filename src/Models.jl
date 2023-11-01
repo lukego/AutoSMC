0dff2b0072f32c51f197b444aaf07072610c83cd
Luke Gorrie <luke@nuddy.net>
2023-11-01 13:58:11 +0100

    It compiles...

--- src/Models.jl
+++ src/Models.jl
@@ -2,13 +2,33 @@ module Models
 
 using Gen
 using GenParticleFilters
+using MacroTools
 
-Config = @NamedTuple{}
+Config = @NamedTuple{response_vars::Vector{Symbol},
+                     fixed_vars::Vector{Symbol}}
 
 Simulation = @NamedTuple{pf_state::ParticleFilterState,
                        config::Config}
 
 Model = @NamedTuple{e::Expr}
+
+function ismodel(expr)
+    @capture(expr, response_ ~ distribution_(parameters__))
+end
+
+@gen function model(config, observations)
+    response ~ oneof(config.response_vars)
+    #distribution ~ distributions
+    μ ~ oneof(config.fixed_vars)
+    σ ~ oneof(config.fixed_vars)
+    for (i,x) in enumerate(observations)
+        {:y=>i} ~ normal(μ, σ)
+    end
+end
+
+@dist oneof(items) = items[uniform_discrete(1, length(items))]
+
+#=
 
 @gen function model()
 end
@@ -34,6 +54,8 @@ end
 @gen function multiply()
     end
 
+=#
+
 # normal(100 + 6 * cores^0.9, sqrt(cores))
 
 #   Constant(k)
@@ -42,11 +64,5 @@ end
 # | Mul(e1, e2)
 
 # (NORMAL (+ 3 (* 1.2 CORES)) 5.1)
-
-
-abst
-
-struct Constant
-
 
 end # module Models
